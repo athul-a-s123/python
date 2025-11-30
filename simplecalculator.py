9060606964aabca359d1e45c3bc8c681dadd08df
Athul A S <athulaslakshmisadan@gmail.com>
2025-11-30 13:49:20 +0530

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,23 +1,22 @@
 a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
-print("Value of a and b are:",a,b)
 print("CHOOSE YOUR OPERATOR")
 print("1.ADD, 2.SUB, 3.MULTIPLY, 4.DIVIDE")
 c=input("Choose any one:")
 if(c=="1"):
   add=a+b
-  print("Addition=",add)
+  print("Result =",a,"+",b,"=",add)
 elif(c=="2"):
   sub=a-b
-  print("Subtraction=",sub)
+  print("Result =",a,"-",b,"=",sub)
 elif(c=="3"):
   mul=a*b
-  print("Multiplication=",mul)
+  print("Result =",a,"×",b,"=",mul)
 elif(c=="4"):
   if(b==0):
-    print("Division by Zero not possible")
+    print("Division by Zero is not possible")
   else:
     div=a/b
-    print("Division=",div)
+    print("Result =",a,"/",b,"=",div)
 else:
   print("INVALID")
