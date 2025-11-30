625960334bf96274d2b0b1d6a27910d63dc42df3
Athul A S <athulaslakshmisadan@gmail.com>
2025-11-30 13:28:29 +0530

    Create simplecalculator.py

---
+++ simplecalculator.py
@@ -0,0 +1,20 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Value of a and b are:",a,b)
+print("CHOOSE YOUR OPERATOR")
+print("1.ADD, 2.SUB, 3.MULTIPLY, 4.DIVIDE")
+c=input("Choose any one:")
+if(c=="1"):
+  add=a+b
+  print("Addition=",add)
+elif(c=="2"):
+  sub=a-b
+  print("Subtraction=",sub)
+elif(c=="3"):
+  mul=a*b
+  print("Multiplication=",mul)
+elif(c=="4"):
+  div=a/b
+  print("Division=",div)
+else:
+  print("INVALID")
